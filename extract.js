e1c0be6ae4538404357b7382f193ccdc8d9a1269
qrztech1-creator <qrztech1@gmail.com>
2026-07-16 20:12:41 -0300

    extract

--- extract.js
+++ extract.js
@@ -1,1 +1,6 @@
-const fs = require('fs'); const file = 'public/pages/inicio.html'; let content = fs.readFileSync(file, 'utf8'); const section = content.substring(content.indexOf('<section id="z1jsXo"'), content.indexOf('</section>', content.indexOf('<section id="z1jsXo"'))); console.log(section);
+const fs=require('fs'); 
+const content = fs.readFileSync('public/pages/inicio.html', 'utf8'); 
+const start = content.indexOf('class="burger'); 
+if (start > -1) { 
+  console.log(content.substring(Math.max(0, start - 20), start + 300)); 
+}
