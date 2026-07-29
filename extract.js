edd10244d969d3f55bd3f03f73b874d98915238a
qrztech1-creator <qrztech1@gmail.com>
2026-07-29 14:08:20 -0300

    atualização banco e ajuste mobile

--- extract.js
+++
@@ -1,6 +0,0 @@
-const fs=require('fs'); 
-const content = fs.readFileSync('public/pages/inicio.html', 'utf8'); 
-const start = content.indexOf('class="burger'); 
-if (start > -1) { 
-  console.log(content.substring(Math.max(0, start - 20), start + 300)); 
-}
